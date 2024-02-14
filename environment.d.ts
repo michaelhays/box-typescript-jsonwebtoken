f779ef18dcf2e9ab89dfd950731bc2d73602acac
Michael Hays <michaelhays36@gmail.com>
2024-02-14 11:43:04 -0600

    Initial commit

---
+++ environment.d.ts
@@ -0,0 +1,7 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    BOX_APP_USER_ID: string
+    BOX_CONFIG: string
+    BOX_FILE_ID: string
+  }
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,56 @@
+import BoxSDK from 'box-node-sdk'
+import { BoxJwtAuth, JwtConfig } from 'box-typescript-sdk-gen/lib/jwtAuth.generated.js'
+
+
+export default function HomePage() {
+  async function boxGetTokenOld() {
+    'use server'
+
+    const boxSDK = BoxSDK.getPreconfiguredInstance(
+      JSON.parse(process.env.BOX_CONFIG),
+    )
+    const boxClient = boxSDK.getAppAuthClient('user', process.env.BOX_APP_USER_ID)
+
+    const token = await boxClient.exchangeToken(
+      ['item_preview'],
+      `https://api.box.com/2.0/files/${process.env.BOX_FILE_ID}`,
+    )
+
+    console.log({ token })
+
+    return token.accessToken
+  }
+
+  async function boxGetTokenNew() {
+    'use server'
+
+    const boxJwtConfig = JwtConfig.fromConfigJsonString(process.env.BOX_CONFIG)
+    const boxJwtAuth = new BoxJwtAuth({ config: boxJwtConfig })
+    boxJwtAuth.asUser(process.env.BOX_APP_USER_ID)
+
+    // Retrieve a token to avoid error: "No access token is available. Make an
+    // API call to retrieve a token before calling this method."
+    await boxJwtAuth.retrieveToken()
+
+    const token = await boxJwtAuth.downscopeToken(
+      ['item_preview'],
+      `https://api.box.com/2.0/files/${process.env.BOX_FILE_ID}`,
+    )
+
+    console.log({ token })
+
+    return token.accessToken
+  }
+
+  return (
+    <main>
+      <form action={boxGetTokenOld}>
+        <button type="submit">box-node-sdk</button>
+      </form>
+
+      <form action={boxGetTokenNew}>
+        <button type="submit">box-typescript-sdk-gen</button>
+      </form>
+    </main>
+  );
+}
